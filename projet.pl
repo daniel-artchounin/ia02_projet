8e5072a376ae9d7f82afc6cdcd1e212fa6746d16
d@ni€| <artchounindaniel@gmail.com>
2016-05-04 10:09:11 +0200

    Reading of a 2-D list.

--- projet.pl
+++ projet.pl
@@ -21,4 +21,17 @@ imprime_2d([TBoard|QBoard], [TPos|QPos]) :- imprime(TBoard, TPos), nl, imprime_2
 imprime([], []).
 imprime([TBoard|QBoard], [TPos|QPos]) :- write(TBoard), write('/'), write(TPos), write(' '), imprime(QBoard, QPos).
 
+% Génération du plateau de jeu initial
 initBoard(C, B, P) :- choixCote(C, B), definePositions(P), imprime_2d(B, P).
+
+% Concatenation of two lists
+concate([], L, L).
+concate([T|Q], L, [T|R]) :- concate(Q, L, R). 
+
+% Read a one dimension list
+read1DList(J, N, T) :- J < N, write(J), write(') Entrez un nombre :'), nl, NewJ is J + 1, read(X), read1DList(NewJ, N, Y), concate([X], Y, T).
+read1DList(J, N, T) :- J =:= N, write(J), write(') Entrez un nombre :'), nl, read(X), T = [X].
+
+% Read a two dimensions list
+read2DList(I, M, N, T) :- I < M, write('*** Ligne '), write(I), nl, NewI is I + 1, read1DList(1, N, Line), read2DList(NewI, M, N, Y), concate([Line], Y, T).
+read2DList(I, M, N, T) :- I =:= M, write('*** Ligne '), write(I), nl, read1DList(1, N, Line), T=[Line].
